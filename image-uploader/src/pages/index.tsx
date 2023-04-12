2d0c468f546d4bb090e8f326317bff096f7415ac
grindcore819 <albert.villeneuve.nguyen@gmail.com>
2023-04-12 13:09:51 -0400

    added T3 and a navbar

---
+++ image-uploader/src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Image Upload Challenge</title>
+        <meta name="description" content="Image Upload Challenge" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        
+      </main>
+    </>
+  );
+};
+
+export default Home;
